2ffdfd2704639bbcfe5e42de9dc417c28fe894f9
MinseokGo <rhalstjr1999@naver.com>
2023-07-24 18:01:40 +0900

    fix: DIP 위배 수정

--- src/test/java/com/example/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/com/example/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,13 +6,14 @@ import com.example.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("빈 이름으로 조회")
